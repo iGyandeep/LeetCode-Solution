d320c2553cf45740a4b7823201a78ccf242ed894
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2023-08-01 23:26:51 +0530

    77. Combinations.cpp
    Company: Facebook, Google, Amazon, Microsoft
    
    Problem Link: https://leetcode.com/problems/combinations/description/

---
+++ 77. Combinations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    vector<vector<int>> ans;
+
+    vector<vector<int>> combine(int n, int k) {
+        vector<int> temp;
+
+        solve(1, n, k, temp);
+
+        return ans;    
+    }
+
+    void solve(int start, int n, int k, vector<int>& temp) {
+        if (k==0) {
+            ans.push_back(temp);
+            return;
+        }
+
+        if (start > n)
+            return;
+
+        temp.push_back(start);
+        solve(start+1, n, k-1, temp);
+
+        temp.pop_back();
+        solve(start+1, n, k, temp);
+    }
+};
