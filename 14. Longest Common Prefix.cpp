822762affdae39fb47731ab39e41bb159c02eccb
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2023-08-04 10:46:52 +0530

    14. Longest Common Prefix.cpp
    Company: Facebook, Google, Apple, Adobe, Amazon, Microsoft, Bloomberg, Uber, Qura, SAP
    
    Problem Link: https://leetcode.com/problems/longest-common-prefix/

--- 14. Longest Common Prefix.cpp
+++ 14. Longest Common Prefix.cpp
@@ -30,3 +30,26 @@ public:
         return ans;
     }
 };
+
+
+/* Approach - 2 */
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string ans = "";
+        sort(strs.begin(), strs.end());
+
+        string a = strs[0];
+        string b = strs[strs.size()-1];
+
+        for (int i=0; i<a.size(); i++) {
+            if (a[i] == b[i])
+                ans.push_back(a[i]);
+            else
+                break;
+        }
+
+        return ans;
+    }
+};
