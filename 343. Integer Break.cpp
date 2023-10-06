8be5e5c316e3784bf2bf0960344f4133205ec499
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2023-10-06 22:39:57 +0530

    343. Integer Break.cpp
    Company: Amazon, Facebook
    
    Problem Link: https://leetcode.com/problems/integer-break/

---
+++ 343. Integer Break.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int t[59];
+    int solve(int n ){
+        
+        if(n == 1)
+            return 1;
+        
+        if(t[n] != -1)
+            return t[n];
+        
+        int result = INT_MIN;
+      
+        for(int i = 1; i<n; i++) {
+            int prod = i * max(n-i, solve(n-i));
+            
+            result = max(result, prod);
+        }
+        
+        return t[n] = result;
+        
+    }
+    
+    int integerBreak(int n) {
+        memset(t, -1, sizeof(t));
+        return solve(n);
+    }
+};
