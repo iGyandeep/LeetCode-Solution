aa0197644b3de9ec51864cedc72f7765b7d5663a
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2023-12-03 22:17:28 +0530

    1266. Minimum Time Visiting All Points.cpp
    Company: Apple
    
    Problem Link: https://leetcode.com/problems/minimum-time-visiting-all-points/

---
+++ 1266. Minimum Time Visiting All Points.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minTimeToVisitAllPoints(vector<vector<int>>& points) {
+        int ans = 0;
+        for(int i = 0 ; i < points.size()-1; i++)
+        {
+            int first = 0;
+            int second = 0;
+            // Index (i, 0)
+            if (points[i][0] < 0 && points[i+1][0] < 0)
+                first = abs( abs(points[i][0]) - abs(points[i+1][0]));
+            else if(points[i][0] < 0 || points[i+1][0] < 0)
+                first = abs( abs(points[i][0]) + abs(points[i+1][0]));
+            else
+                first = abs( abs(points[i][0]) - abs(points[i+1][0]));
+            // Index (i, 1)
+            if(points[i][1] < 0 && points[i+1][1] < 0)
+                second = abs( abs(points[i][1]) - abs(points[i+1][1]));
+            else if(points[i][1] < 0 || points[i+1][1] < 0)
+                second = abs( abs(points[i][1]) + abs(points[i+1][1]));
+            else
+                second = abs( abs(points[i][1]) - abs(points[i+1][1]));
+            ans += max(first,second);
+        }
+        return ans;
+    }
+};
