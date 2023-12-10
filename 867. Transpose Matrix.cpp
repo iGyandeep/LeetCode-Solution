53ea6b3dc559ae8a4cdd4932aba165c62a8cfcc9
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2023-12-10 22:51:39 +0530

    867. Transpose Matrix.cpp
    Company: Apple, Amazon, Microsoft, Citadel
    
    Problem Link: https://leetcode.com/problems/transpose-matrix/description/

---
+++ 867. Transpose Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        vector<vector<int>> result(m, vector<int>(n,0));
+        
+        for(int i=0; i<n; i++)
+        {
+            for (int j=0; j<m; j++)
+            {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
+    }
+};
