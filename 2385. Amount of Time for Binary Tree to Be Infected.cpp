4ea6780106c42a928cb250c7ad04ecff13b212a9
Gyandeep <53125532+iGyandeep@users.noreply.github.com>
2024-01-10 23:40:25 +0530

    2385. Amount of Time for Binary Tree to Be Infected.cpp
    Company: 
    
    Problem Link: https://leetcode.com/problems/amount-of-time-for-binary-tree-to-be-infected/description/

---
+++ 2385. Amount of Time for Binary Tree to Be Infected.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int result = INT_MIN;
+
+    int solve(TreeNode* root, int start) {
+        if(root == NULL) {
+            return 0;
+        }
+
+        int LH = solve(root->left, start);
+        int RH = solve(root->right, start);
+
+        if(root->val == start) {
+            result = max(LH, RH);
+            return -1;
+        } else if(LH >= 0 && RH >= 0) {
+            return max(LH, RH) + 1;
+        } else {
+            int d = abs(LH) + abs(RH);
+            result = max(result, d);
+
+            return min(LH, RH) - 1;
+        }
+
+        return 0;
+    }
+
+    int amountOfTime(TreeNode* root, int start) {
+        solve(root, start);
+
+        return result;
+    }
+};
